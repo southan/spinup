7303bd6b9f79107f45a4921aa440e93db807bdf5
Alex Southan <alex@southan.dev>
2026-01-11 20:40:49 +0000

    Improve jail status output formatting

--- commands/jail/status.sh
+++ commands/jail/status.sh
@@ -4,7 +4,16 @@
 jail=${args[jail]}
 
 if [[ -n $jail ]]; then
-	sudo fail2ban-client status "$jail"
+	jail_status=$(sudo fail2ban-client status "$jail")
+	ip_list=$(echo "$jail_status" | grep 'Banned IP list:' | sed 's/.*Banned IP list:[[:space:]]*//' | tr ' ' '\n')
+
+	echo "$jail_status" | grep -oP '(Current|Total).+' | column -t -s '	'
+
+	if [[ -n $ip_list ]]; then
+		count=$(echo "$ip_list" | wc -l)
+
+		echo "$ip_list" | nl -w${#count} -s ') ' | column
+	fi
 else
 	for jail in $(sudo fail2ban-client status | grep 'Jail list:' | sed -E 's/^[^:]+:[ \t]+//' | tr ',' ' '); do
 		echo "$(status_marker 0) $jail"
